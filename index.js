2d1e6c1a734c2a4bdd822ee27d2c11d842176cbf
Rasujon3 <rasujon3@gmail.com>
2022-06-02 20:57:09 +0600

    state,action

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+// defining constants
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const ADD_USER = "ADD_USER";
+
 // state
+const initialCounterState = {
+  count: 0,
+};
+
+const initialUsersState = {
+  users: [{ name: "anisul islam" }],
+};
+
+// action - Object -> i. type, ii. payload
+// INCREMENT COUNTER
+const incrementCounter = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+// DECREMENT COUNTER
+const decrementCounter = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const addUser = () => {
+  return {
+    type: ADD_USER,
+    payload: { name: "shakil" },
+  };
+};
+
+// incrementCounter()
+
+// 1. state
+// 2. dispatch -> action
+// 3. reducer -> based on action
+// 4. store update
