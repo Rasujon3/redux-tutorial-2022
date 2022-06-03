dc595de1fab2630e21166cf3088b79ab7ddcd6b3
Rasujon3 <rasujon3@gmail.com>
2022-06-03 19:53:23 +0600

    middleware

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 // productReducer
 
-const { createStore, combineReducers } = require("redux");
+const { createStore, applyMiddleware } = require("redux");
+const { default: logger } = require("redux-logger");
 
 // product Constant
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCTS = "ADD_PRODUCTS";
 
-// cart Constant
-const GET_CART_ITEMS = "GET_CART_ITEMS";
-const ADD_CART_ITEM = "ADD_CART_ITEM";
-
 // Product states
 const initialProductState = {
   products: ["sugar", "salt"],
   numberOfProducts: 2,
-};
-
-// Cart states
-const initialCartState = {
-  cart: ["sugar"],
-  numberOfProducts: 1,
 };
 
 // product actions
@@ -33,20 +24,6 @@ const addProduct = (product) => {
   return {
     type: ADD_PRODUCTS,
     payload: product,
-  };
-};
-
-// cart actions
-const getCart = () => {
-  return {
-    type: GET_CART_ITEMS,
-  };
-};
-
-const addCart = (cart) => {
-  return {
-    type: ADD_CART_ITEM,
-    payload: cart,
   };
 };
 
@@ -68,40 +45,11 @@ const productReducer = (state = initialProductState, action) => {
   }
 };
 
-// cartReducer
-const cartReducer = (state = initialCartState, action) => {
-  switch (action.type) {
-    case GET_CART_ITEMS:
-      return {
-        ...state,
-      };
-    case ADD_CART_ITEM:
-      return {
-        cart: [...state.cart, action.payload],
-        numberOfProducts: state.numberOfProducts + 1,
-      };
-
-    default:
-      return state;
-  }
-};
-
-// rootReducer
-
-const rootReducer = combineReducers({
-  productR: productReducer,
-  cartR: cartReducer,
-});
-
 // store
-// const store = createStore(productReducer);
-const store = createStore(rootReducer);
+const store = createStore(productReducer, applyMiddleware(logger));
 store.subscribe(() => {
   console.log(store.getState());
 });
 
 store.dispatch(getProducts());
 store.dispatch(addProduct("pen"));
-
-store.dispatch(getCart());
-store.dispatch(addCart("pen"));
